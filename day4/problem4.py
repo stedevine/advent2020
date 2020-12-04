8112dca1b6c78c2066305f189c49be42a24ff0c3
Stephen Devine <stephen.devine@bigfishgames.com>
2020-12-03 22:58:03 -0800

    clean up, use list of functions

--- day4/problem4.py
+++ day4/problem4.py
@@ -1,40 +1,59 @@
-def count_valid(problem_data):
-    passport = {}
-    valid = 0 
+# conditions is a list of functions that take a 
+# passport (dictionary) and returns a boolean
+def count_valid(problem_data, conditions):
+    number_valid = 0
+    is_valid = True 
+    for passport in get_passports(problem_data):
+        for condition in conditions:
+            if not condition(passport):
+                is_valid = False
+                break
+        if is_valid:
+            number_valid += 1
+        is_valid = True
 
+    return number_valid  
+
+def get_passports(problem_data):
+    passports = []
+    passport = {}
     for line in problem_data:
         if line is not '':
             populate_passport(passport, line)
-        else: 
-            if is_valid(passport):
-                valid += 1
+        else:
+            # line break - add the passport to the list
+            passports.append(passport)
             passport = {}
 
-    if is_valid(passport):
-        valid += 1
-    
-    print(valid)
+    # end of file, add the last passport to the list
+    passports.append(passport)
+
+    return passports
 
 def populate_passport(passport,line):
     fields = line.split(' ')
     for field in fields:
         passport[field.split(':')[0]] = field.split(':')[1]
 
-def is_valid(passport):
-    if 'byr' in passport and 'iyr' in passport and 'eyr' in passport and 'hgt' in passport and 'hcl' in passport and 'ecl' in passport and 'pid' in passport:
-        #print(check_integer(passport, 'byr', 1920, 2002))
-        return check_integer(passport, 'byr', 1920, 2002) \
-            and check_integer(passport, 'iyr', 2010, 2020) \
-            and check_integer(passport, 'eyr', 2020, 2030) \
-            and check_height(passport) \
-            and check_hair(passport) \
-            and check_eye(passport) \
-            and check_number(passport)
+def has_fields(passport):
+    for field in ['byr', 'iyr', 'eyr','hgt','hcl','ecl','pid']:
+        if not field in passport:
+            return False
+    return True
+
+def fields_are_valid(passport):
+    return check_integer(passport, 'byr', 1920, 2002) \
+        and check_integer(passport, 'iyr', 2010, 2020) \
+        and check_integer(passport, 'eyr', 2020, 2030) \
+        and check_height(passport) \
+        and check_hair(passport) \
+        and check_eye(passport) \
+        and check_number(passport)
 
 def check_integer(passport, key, min, max):
     try:
-        v = int(passport[key])
-        return v >= min and v <= max
+        val = int(passport[key])
+        return val >= min and val <= max
     
     except ValueError:
         return False
@@ -82,8 +101,8 @@ def check_number(passport):
     except ValueError:
         return False 
 
-
 problem_input = []
 with open('./problem4_input.txt') as f:
     problem_input = [l.strip() for l in f.readlines()]
-    count_valid(problem_input)
+    print('Problem 1 : {}'.format(count_valid(problem_input,[has_fields])))
+    print('Problem 2 : {}'.format(count_valid(problem_input,[has_fields,fields_are_valid])))
