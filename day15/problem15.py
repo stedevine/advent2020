9fd159026bb0ca22e05722b55b5b07a11a191d6b
Stephen Devine <stephen.devine@bigfishgames.com>
2020-12-22 22:36:05 -0800

    day15

--- day15/problem15.py
+++ day15/problem15.py
@@ -8,7 +8,7 @@ def init(input):
     return history, turn, input[-1]
 
 def get_next(history, turn, number):
-    if turn % 100000 == 0 :
+    if turn % 10000000 == 0 :
         print(turn)
 
     result = 0
@@ -18,44 +18,32 @@ def get_next(history, turn, number):
         result = history[number][0] - history[number][1]
 
     if result in history:
-        history[result].insert(0, turn)
+        if len(history[result]) == 1:
+            history[result].insert(0,turn)
+        else:
+            history[result][1] = history[result][0]
+            history[result][0] = turn
+        #history[result].insert(0, turn)
     else:
         history[result] = [turn]
 
     #print('turn {} input {} output {} history {}'.format(turn, number, result, history))
     return history, result
 
-
-'''    
-    result = 0
-    if turn == 5:
-        print('turn 5 history {}'.format(history))
-    if number in history:
-        if turn == 5:
-            print('{} is in history'.format(number))
-        history[number].insert(0,turn)
-        if len(history[number]) > 2:
-            result = history[number][1] - history[number][2]
-            #return (history, history[number][1] - history[number][2])
     
-    else:
-        # First time
-        history[number] = [turn]
-    
-    print('turn {} input {} output {}'.format(turn, number, result))
-    return (history, result)
- '''   
-    
+def get_nth(n):
+    input = [13,0,10,12,1,5,8]
 
-#input = [13,0,10,12,1,5,8]
-input = [0,3,6]
-history, turn, number = init(input)
-print(history)
-result = input[-1]
+    history, turn, number = init(input)
+    #print(history)
+    result = input[-1]
 
-for turn in range(len(input) + 1,30000001):
-    (history, result) = get_next(history, turn, result)
+    for turn in range(len(input) + 1,n+1):
+        (history, result) = get_next(history, turn, result)
 
-    #print('{}:{} ({})'.format(turn,number,history)
+        #print('{}:{} ({})'.format(turn,number,history)
 
-print(result)
+    print(result)
+
+get_nth(2020)
+get_nth(30000000)
